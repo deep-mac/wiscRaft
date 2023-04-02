9afb523df91802d723725462e2b667b7e7ba4799
vramadas <vramadas@node1.p2-group-9.advosuwmadison-pg0.utah.cloudlab.us>
2023-04-02 00:41:04 -0600

    reverted some renaming changes

--- log/log.cc
+++ log/log.cc
@@ -2,40 +2,34 @@
 
 //Should be called atomically for each append
 void Log::LogAppend(LogEntry& entry){
-  logLock.lock();
   log.push_back(entry);
-  //fh.open("log.txt",std::fstream::app);
-  std::string line = std::to_string(entry.isRead) + "$" + entry.key + "$" + std::to_string(entry.value) + "$" + std::to_string(entry.commandID);
+  fh.open("log.txt",std::fstream::app);
+  std::string line = std::to_string(entry.GetOrPut) + "$" + entry.key + "$" + std::to_string(entry.value) + "$" + std::to_string(entry.commandID);
   fh << line << std::endl;
-  //fh.close();
-  logLock.unlock();
+  fh.close();
 }
 
 void Log::LogAppend(bool command, std::string newKey, int newValue, uint32_t newID) {
 
-  logLock.lock();
   LogEntry entry;
   entry.setEntry(command, newKey, newValue, newID);
   log.push_back(entry);
-  //fh.open("log.txt",std::fstream::app);
+  fh.open("log.txt",std::fstream::app);
   std::string line = std::to_string(command) + "$" + newKey + "$" + std::to_string(newValue) + "$" + std::to_string(newID);
   fh << line << std::endl;
-  //fh.close();
-  logLock.unlock();
+  fh.close();
 }
 
 
 //Should be called atomically once for every execute
 void Log::LogCleanup(){
-  logLock.lock();
   log.pop_front();
   fh.open("log.txt",std::fstream::trunc);
   for(LogEntry val : log){
-   std::string line = std::to_string(val.isRead) + "$" + val.key + "$" + std::to_string(val.value) + "$" + std::to_string(val.commandID);
+   std::string line = std::to_string(val.GetOrPut) + "$" + val.key + "$" + std::to_string(val.value) + "$" + std::to_string(val.commandID);
    fh<<line<<std::endl;
   }
   fh.close();
-  logLock.unlock();
 }
 
 //Used to get the head for execution
@@ -46,13 +40,12 @@ LogEntry Log::get_head(){
 //Needed for debug purposes
 void Log::print(){
  for(LogEntry val:log){
-  std::string line = std::to_string(val.isRead) + "$" + val.key + "$" + std::to_string(val.value) + "$" + std::to_string(val.commandID);
+  std::string line = std::to_string(val.GetOrPut) + "$" + val.key + "$" + std::to_string(val.value) + "$" + std::to_string(val.commandID);
   std::cout<<line<<std::endl;
  }
 }
 
 void Log::commitEntry(uint32_t commandID) {
-    logLock.lock();
     assert(commitID < log.size());
     if (commandID != log[commitID].commandID) {
     // Shouldn't be here. The commandID passed should always be the same as that of the log entry waiting to commit
@@ -60,14 +53,13 @@ void Log::commitEntry(uint32_t commandID) {
        return;
     }
     commitID++;
-    logLock.unlock();
 }
 
 int Log::executeEntry() {
     int ret = 0;
     assert(commitID > 0);
     LogEntry entry = get_head();
-    if (entry.isRead) {
+    if (entry.GetOrPut) {
 	ret = database.get(entry.key);
     } else {
     	ret = database.put(entry.key, entry.value);
@@ -78,7 +70,7 @@ int Log::executeEntry() {
 }
 
 void LogEntry::setEntry(bool command, std::string newKey, int newValue, uint32_t newID) {
-    isRead = command;
+    GetOrPut = command;
     key = newKey;
     value = newValue;
     commandID = newID;
@@ -88,8 +80,8 @@ void LogEntry::setEntry(bool command, std::string newKey, int newValue, uint32_t
    LogEntry entry[4];
    Log log;
 
-   entry[0].isRead = false;
-   entry[2].isRead = false;
+   entry[0].GetOrPut = false;
+   entry[2].GetOrPut = false;
 
    for(int i=0; i<4; i++)
     log.LogAppend(entry[i]);
---
+++ log/log.h
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<deque>
+#include<string>
+#include<fstream>
+#include<mutex>
+#include "database.h"
+#include <limits.h>
+#include <cassert>
+
+class LogEntry{
+ public:
+
+ bool GetOrPut;
+ std::string key;
+ int value;
+ uint32_t commandID;
+
+ LogEntry() : GetOrPut(true),
+	      key(""),
+	      value(0),
+	      commandID(INT_MAX) {}
+
+ LogEntry(const LogEntry& copy) {
+  GetOrPut = copy.GetOrPut;
+  key = copy.key;
+  value = copy.value;
+  commandID = copy.commandID;
+ }
+};
+
+class Log {
+
+ std::deque<LogEntry> log;
+ std::fstream fh;
+ uint32_t commitID;
+ Database database;
+ public:
+
+ Log() : database() {
+    commitID = 0;
+ }
+
+ //Should be called atomically for each append
+ void LogAppend(LogEntry& Entry);
+
+ //Should be called atomically once for every execute
+ void LogCleanup();
+
+ //Used to get the head for execution
+ LogEntry get_head();
+
+ void commitEntry(uint32_t commandID);
+
+ int executeEntry();
+
+ //Needed for debug purposes
+ void print();
+};
