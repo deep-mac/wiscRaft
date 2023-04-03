d207734387e39d0f52dfa65c46784a55f2394c4a
vramadas <vramadas@node0.p2-group-9.advosuwmadison-pg0.utah.cloudlab.us>
2023-04-02 18:25:39 -0600

    modified log to do lots of stuff

--- log/log.cc
+++ log/log.cc
@@ -1,76 +1,24 @@
 #include"log.h"
 
-//Should be called atomically for each append
-void Log::LogAppend(LogEntry& entry){
-  log.push_back(entry);
-  fh.open("log.txt",std::fstream::app);
-  std::string line = std::to_string(entry.GetOrPut) + "$" + entry.key + "$" + std::to_string(entry.value) + "$" + std::to_string(entry.commandID);
-  fh << line << std::endl;
-  fh.close();
-}
+int main(){
+   LogEntry entry[4];
+   Log log;
 
-void Log::LogAppend(bool command, std::string newKey, int newValue, uint32_t newID) {
+   entry[0].GetOrPut = 1;
+   entry[2].GetOrPut = 1;
 
-  LogEntry entry;
-  entry.setEntry(command, newKey, newValue, newID);
-  log.push_back(entry);
-  fh.open("log.txt",std::fstream::app);
-  std::string line = std::to_string(command) + "$" + newKey + "$" + std::to_string(newValue) + "$" + std::to_string(newID);
-  fh << line << std::endl;
-  fh.close();
-}
+   for(int i=0; i<4; i++)
+    log.LogAppend(entry[i]);
 
+   //Do Execution here, do not pop.
+   LogEntry exec = log.get_head();
+   //Execute the entry based on conditions
+   log.print();
 
-//Should be called atomically once for every execute
-void Log::LogCleanup(){
-  log.pop_front();
-  fh.open("log.txt",std::fstream::trunc);
-  for(LogEntry val : log){
-   std::string line = std::to_string(val.GetOrPut) + "$" + val.key + "$" + std::to_string(val.value) + "$" + std::to_string(val.commandID);
-   fh<<line<<std::endl;
-  }
-  fh.close();
-}
+   for(int i=0; i<4; i++)
+    log.LogCleanup();
 
-//Used to get the head for execution
-LogEntry Log::get_head(){
-  return log.front(); 
-}
+   log.print();
 
-//Needed for debug purposes
-void Log::print(){
- for(LogEntry val:log){
-  std::string line = std::to_string(val.GetOrPut) + "$" + val.key + "$" + std::to_string(val.value) + "$" + std::to_string(val.commandID);
-  std::cout<<line<<std::endl;
- }
-}
-
-void Log::commitEntry(uint32_t commandID) {
-    assert(commitID < log.size());
-    if (commandID != log[commitID].commandID) {
-    // Shouldn't be here. The commandID passed should always be the same as that of the log entry waiting to commit
-       std::cout << __FILE__ << ":" << __LINE__ << ". Incorrect commandID received! Returing without doing anything" << std::endl;
-       return;
-    }
-    commitID++;
-}
-
-int Log::executeEntry() {
-    int ret = 0;
-    assert(commitID > 0);
-    LogEntry entry = get_head();
-    if (entry.GetOrPut) {
-	ret = database.get(entry.key);
-    } else {
-    	ret = database.put(entry.key, entry.value);
-    }
-    commitID--;
-    return ret;
-}
-
-void LogEntry::setEntry(bool command, std::string newKey, int newValue, uint32_t newID) {
-    GetOrPut = command;
-    key = newKey;
-    value = newValue;
-    commandID = newID;
+   return 0;
 }
--- log/log.h
+++ log/log.h
@@ -1,61 +1,197 @@
+#pragma once
+
 #include<iostream>
 #include<deque>
 #include<string>
+#include<array>
 #include<fstream>
-#include<mutex>
-#include "database.h"
-#include <limits.h>
-#include <cassert>
+#include<sstream>
 
 class LogEntry{
  public:
 
- bool GetOrPut;
+ int GetOrPut;
  std::string key;
  int value;
- uint32_t commandID;
+ int command_id;
+ int command_term;
+ int client_id;                    //Command ID from client perspective
+ std::array<bool,3> matched;       //To indicate whether peer server has acknowledged, we are gonna assume 3 servers for this project
 
- LogEntry() : GetOrPut(true),
-	      key(""),
-	      value(0),
-	      commandID(INT_MAX) {}
+ LogEntry(){
+  for(int i=0; i<3; i++)
+   matched[i] = false;
+ }
 
- LogEntry(const LogEntry& copy) {
+ LogEntry(const LogEntry& copy){
   GetOrPut = copy.GetOrPut;
   key = copy.key;
   value = copy.value;
-  commandID = copy.commandID;
+  command_id = copy.command_id;
+  command_term = copy.command_term;
+  client_id = copy.client_id;
+  matched = copy.matched; 
  }
-
- void setEntry(bool command, std::string newKey, int newValue, uint32_t newID);
 };
 
-class Log {
+class Log{
 
  std::deque<LogEntry> log;
- std::fstream fh;
- uint32_t commitID;
- LogDatabase database;
+ std::ofstream fout;
+ std::ifstream fin;
+
  public:
 
- Log() : database() {
-    commitID = 0;
+ int LastApplied;
+ int nextIdx;
+ int commitIdx;
+ int matchIdx;
+
+ Log(){
+   //Initializing LastApplied on boot/crash comeback
+   LastApplied = 0;
+   fin.open("applied.txt",std::fstream::in);
+   std::string line;
+   if(getline(fin,line))
+    LastApplied = stoi(line);
+   fin.close();
+  
+   //Initializing log on boot/crash comeback 
+   LogEntry copy;
+   int i;
+   nextIdx = 1;
+   fin.open("log.txt",std::fstream::in);
+   for(i=1; getline(fin,line); i++){
+    if(i >= LastApplied+1){
+     std::stringstream stream(line);
+     std::string temp;
+     getline(stream,temp,'$');
+     copy.GetOrPut = stoi(temp);
+     getline(stream,temp,'$');
+     copy.key = temp;
+     getline(stream,temp,'$');
+     copy.value = stoi(temp);
+     getline(stream,temp,'$');
+     copy.command_id = stoi(temp);
+     getline(stream,temp,'$');
+     copy.command_term = stoi(temp);
+     getline(stream,temp,'\n');
+     copy.client_id = stoi(temp);
+     log.push_back(copy);     
+    }
+   }
+   nextIdx = i;
+   fin.close();
+
+   //Initializing commitIdx on boot/crash comeback
+   commitIdx = 0;
+   matchIdx = 0;
+   fin.open("commit.txt",std::fstream::in);
+   if(getline(fin,line)){
+    commitIdx = stoi(line);
+    matchIdx = commitIdx;
+   }
+   fin.close();
  }
 
  //Should be called atomically for each append
- void LogAppend(LogEntry& Entry);
- void LogAppend(bool command, std::string newKey, int newValue, uint32_t newID);
+ void LogAppend(LogEntry& Entry){
+   log.push_back(Entry);
+   nextIdx++;
+   fout.open("log.txt",std::fstream::app);
+   std::string line = std::to_string(Entry.GetOrPut) + "$" + Entry.key + "$" + std::to_string(Entry.value) + "$" + std::to_string(Entry.command_id) + "$" + std::to_string(Entry.command_term) + "$" + std::to_string(Entry.client_id);
+   fout<<line<<std::endl;
+   fout.close();
+ }
 
  //Should be called atomically once for every execute
- void LogCleanup();
+ void LogCleanup(){
+   log.pop_front();
+   LastApplied++;
+
+   fout.open("applied.txt",std::fstream::trunc);
+   fout<<std::to_string(LastApplied)<<std::endl;
+   fout.close();
+
+   //Why? : If we un-persist the data on leader after execution (which happend on majority), some straggler server (not part of majority) might need it after deleting the entry which is wrong, so commenting this.
+   /*
+   fout.open("log.txt",std::fstream::trunc);
+   for(LogEntry val : log){
+    std::string line = std::to_string(val.GetOrPut) + "$" + val.key + "$" + std::to_string(val.value) + "$" + std::to_string(val.command_id) + "$" + std::to_string(val.command_term) + "$" + std::to_string(val.client_id);
+    fout<<line<<std::endl;
+   }
+   fout.close();
+   */
+ }
 
  //Used to get the head for execution
- LogEntry get_head();
+ LogEntry get_head(){
+   return log.front(); 
+ }
 
- void commitEntry(uint32_t commandID);
+ //Used to get the tail for checking
+ LogEntry get_tail(){
+   return log.back();
+ }
 
- int executeEntry();
+ //Used to access a index of a log
+ LogEntry& get_entry(int idx){
+   return log[idx];
+ }
+
+ //Used to get the entry from file log
+ LogEntry& get_file_entry(int idx){
+  LogEntry copy;
+  std::string line;
+
+  fin.open("log.txt",std::fstream::in);
+  for(int i=1; getline(fin,line); i++){
+   if(i == idx){
+    std::stringstream stream(line);
+    std::string temp;
+    getline(stream,temp,'$');
+    copy.GetOrPut = stoi(temp);
+    getline(stream,temp,'$');
+    copy.key = temp;
+    getline(stream,temp,'$');
+    copy.value = stoi(temp);
+    getline(stream,temp,'$');
+    copy.command_id = stoi(temp);
+    getline(stream,temp,'$');
+    copy.command_term = stoi(temp);
+    getline(stream,temp,'\n');
+    copy.client_id = stoi(temp);
+    break;
+   }
+  }
+  fin.close();
+
+  return copy;
+ }
+
+ //To set the matched bit based on the server
+ void set_matched(int LogIdx, int ServerIdx){
+   log[LogIdx].matched[ServerIdx] = true;
+ }
+
+ //Used to get the size of log
+ int get_size(){
+   return log.size();  
+ }
+
+ //Need to persist atomically
+ void persist_commitIdx(){
+   fout.open("commit.txt",std::fstream::trunc);
+   std::string line = std::to_string(commitIdx);
+   fout<<line<<std::endl;
+   fout.close();
+ }
 
  //Needed for debug purposes
- void print();
+ void print(){
+  for(LogEntry val:log){
+   std::string line = std::to_string(val.GetOrPut) + "$" + val.key + "$" + std::to_string(val.value) + "$" + std::to_string(val.command_id) + "$" + std::to_string(val.command_term) + "$" + std::to_string(val.client_id);
+   std::cout<<line<<std::endl;
+  }
+ }
 };
